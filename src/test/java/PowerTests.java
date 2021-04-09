dc5f0088270954870569f8e9d7715c856bd4285e
svafaiet <svafaiet@gmail.com>
2021-04-09 23:04:56 +0430

    Add Power tests

---
+++ src/test/java/PowerTests.java
@@ -0,0 +1,53 @@
+
+import Bridge.Multiplier;
+import Bridge.MultiplierFor;
+import Bridge.MultiplierNorm;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PowerTests {
+
+    @Test
+    void testMultiplierFor() {
+        Multiplier multiplier = new MultiplierFor();
+        assertEquals(20, multiplier.mult(2, 10));
+
+    }
+
+    @Test
+    void testMultiplierNorm() {
+        Multiplier multiplier = new MultiplierFor();
+        assertEquals(20, multiplier.mult(2, 10));
+    }
+
+    @Test
+    void testPowerRecursive() {
+        ArrayList<Multiplier> multiplierArrayList = new ArrayList<Multiplier>() {
+            {
+                add(new MultiplierFor());
+                add(new MultiplierNorm());
+            }
+        };
+        for (Multiplier multiplier : multiplierArrayList) {
+            PowerCalc powerCalc = new PowerRecursive(multiplier);
+            assertEquals(powerCalc.pow(6, 5), 7776);
+        }
+    }
+
+    @Test
+    void testPowerFor() {
+        ArrayList<Multiplier> multiplierArrayList = new ArrayList<Multiplier>() {
+            {
+                add(new MultiplierFor());
+                add(new MultiplierNorm());
+            }
+        };
+        for (Multiplier multiplier : multiplierArrayList) {
+            PowerCalc powerCalc = new PowerFor(multiplier);
+            assertEquals(powerCalc.pow(6, 5), 7776);
+        }
+    }
+}
